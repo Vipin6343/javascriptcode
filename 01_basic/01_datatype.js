465fb060893c1fce5f0634dd6568fc1b62ff3209
Vipin6343 <137335320+Vipin6343@users.noreply.github.com>
2024-04-12 09:09:43 +0000

    data

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,24 @@
+"user strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs not  browser
+let a= 76
+let b=" lohli"
+let islogin=false
+
+
+//type of is used to know the datatype of the variable like
+console.lof(typeof"kohli" )
+
+
+
+//data type are 
+//number 
+//bigInt
+//string 
+// boolean
+//null // standaralone value
+//undefined
+//symbol
+// these are primitive data type
+
+ 
