d69f95ad138987ead879fd3c3e55426d7925f440
Vipin6343 <137335320+Vipin6343@users.noreply.github.com>
2024-04-15 17:44:13 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,14 @@
+//array
+const myarray=[10,20,30,40,50,"vipin ","virat"]
+console.log(myarray[2])
+const cricketer=["virat","rohit","pant","bumrah",]
+
+
+const array2=new Array(1,2,3,4,4,5,5,6,6,7,7,8,8,9,9,0,5)
+// array method
+// Push()
+// Pop()
+// unshift()
+// shift()
+// slice()
+// splice()
