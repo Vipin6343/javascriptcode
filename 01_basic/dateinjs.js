5ef5ae71180db17cfe7fd35cea22276f1ca33c8e
Vipin6343 <137335320+Vipin6343@users.noreply.github.com>
2024-04-15 16:17:20 +0000

    date

---
+++ 01_basic/dateinjs.js
@@ -0,0 +1,25 @@
+// date are calculated in millisecond
+
+// let mydate=new Date();
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.getDate())
+// console.log(mydate.getFullYear())
+// console.log(mydate.getHours())
+// console.log(mydate.getMinutes())
+// console.log(mydate.toLocaleString())
+
+
+let mycreatedate=new Date()
+console.log(Date.now())
+// the result will come into the milli second 
+// to convert miili second into the second we use the method
+
+console.log(Math.floor(Date.now()/1000))
+console.log(typeof mycreatedate)
+console.log(mycreatedate.getMonth()+1)
+
+
+
+
+
